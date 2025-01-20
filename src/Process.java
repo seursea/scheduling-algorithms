0d803642b716721e15156977a708cb8e970aebfb
Aldrei Justin Santua <main.aldreijustin.santua@cvsu.edu.ph>
2025-01-20 10:32:46 +0800

    final commit

--- src/Process.java
+++ src/Process.java
@@ -1,9 +1,9 @@
 public class Process {
-   String processID;      // Unique identifier for the process
-   int arrivalTime;       // Time the process arrives in the system
-   int burstTime;         // Duration required by the process for execution
-   int completionTime;    // Time when the process completes execution
-   int turnAroundTime;    // Time from process arrival to completion
+    String processID;      // Unique identifier for the process
+    int arrivalTime;       // Time the process arrives in the system
+    int burstTime;         // Duration required by the process for execution
+    int completionTime;    // Time when the process completes execution
+    int turnAroundTime;    // Time from process arrival to completion
     int waitingTime;       // Time the process spends waiting in the ready queue
     int startTime;         // Time when the process starts execution
 
